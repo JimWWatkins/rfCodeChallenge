e43ff81155724d306067b2f3442158f897169536
Jim Watkins <jimwatkins@Jims-MacBook-Pro.local>
2023-02-10 12:12:58 -0700

    setup before and after hooks to create and quit driver

---
+++ e2e/src/stepDefinitions/setup/hooks.ts
@@ -0,0 +1,16 @@
+import { Before, After } from "@cucumber/cucumber"
+import { Builder } from 'selenium-webdriver'
+
+Before(
+    async function() {
+        const driver = new Builder().forBrowser("chrome").build();
+        await driver.manage().window().maximize();
+        global.myDriver = driver;
+    }
+)
+
+After(
+    async function() {
+        await global.myDriver.quit();
+    }    
+)
